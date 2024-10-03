6608d5f508dba7f3824c474ee3dc5c263bc6f4ee
WhannJaii <chatwimonw5@gmail.com>
2024-10-03 22:49:53 +0700

    data ตัวอย่าง

---
+++ คำสั่ง SQL/script insert db_art_toy product.sql
@@ -0,0 +1,10 @@
+INSERT INTO PRODUCT (prod_id, prod_name, prod_size, prod_amount, prod_price, prod_img, type_id) VALUES
+(1, 'baby tree v.3 ยกกล่อง', 'ขนาดสูงประมาณ 20 cm', 6, 2700, NULL, 1),
+(2, 'baby tree v.3 เดียว', 'ขนาดสูงประมาณ 20 cm', 1, 520, NULL, 2),
+(3, 'baby tree v.2 ยกกล่อง', 'ขนาดสูงประมาณ 15 cm', 8, 2330, NULL, 1),
+(4, 'baby tree v.2 เดียว', 'ขนาดสูงประมาณ 15 cm', 1, 300, NULL, 2),
+(5, 'baby tree v.1 ยกกล่อง', 'ขนาดสูงประมาณ 15 cm', 6, 2050, NULL, 1),
+(6, 'baby tree v.1 เดียว', 'ขนาดสูงประมาณ 15 cm', 1, 270, NULL, 2),
+(7, 'migo เดียว', 'ขนาดสูงประมาณ 20-30 cm', 1, 290, NULL, 2),
+(8, 'kimmon v.6 ยกกล่อง', 'ขนาดสูงประมาณ 20 cm', 6, 2010, NULL, 1),
+(9, 'kimmon v.6 เดียว', 'ขนาดสูงประมาณ 20 cm', 1, 330, NULL, 2);
